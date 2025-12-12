d6a74f4b97116013624946f609203272079125bc
Conner Homan <connermhoman@gmail.com>
2025-12-12 18:05:39 -0500

    Bug fix

--- Assets/Scripts/TitleScreen.cs
+++ Assets/Scripts/TitleScreen.cs
@@ -3,7 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class TitleScreen : MonoBehaviour
 {
-    public string sceneToLoad = "scene_9"; // change this to your actual gameplay scene name
+    public string sceneToLoad = "scene_8"; // change this to your actual gameplay scene name
 
     void Update()
     {
